ecfb9f9bbb02f024a5816a972fb8a375f30b63f0
Samermadieh <smadieh@gmail.com>
2026-02-26 16:08:39 -0500

    Add headset and controller tracking nativelly using pure OpenXR bindings.

--- Assets/Scripts/XRRig.cs
+++ Assets/Scripts/XRRig.cs
@@ -1,16 +1,31 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem.XR;
 
 public class XRRig : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private Transform head;
+    [SerializeField] private Transform leftHand;
+    [SerializeField] private Transform rightHand;
+    XRActions _actions;
+
+    private void Awake()
     {
-        
+        _actions = new XRActions();
+        _actions.Enable();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        var headPosition = _actions.Actions.HeadPosition.ReadValue<Vector3>();
+        var headRotation = _actions.Actions.HeadRotation.ReadValue<Quaternion>();
+        var left = _actions.Actions.LeftPose.ReadValue<PoseState>();
+        var right = _actions.Actions.RightPose.ReadValue<PoseState>();
+        head.position = headPosition;
+        head.rotation = headRotation;
+        leftHand.position = left.position;
+        leftHand.rotation = left.rotation;
+        rightHand.position = right.position;
+        rightHand.rotation = right.rotation;
     }
 }
